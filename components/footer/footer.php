e067d186f92c126d2e6cad1a47837f91ade054ac
SeanMee86 <seanmee8186@gmail.com>
2017-07-27 12:03:05 -0700

    authorization for privileges set

---
+++ components/footer/footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: seanm
+ * Date: 7/27/2017
+ * Time: 11:58 AM
+ */
+?>
+
+
+
+
+</body>
+</html>
--- dashboard/index.php
+++ dashboard/index.php
@@ -5,6 +5,26 @@ if(isset($_SESSION['user_info'])){
     $user_info = $_SESSION['user_info'];
 
     echo 'Welcome ' . $user_info['first_name'];
+?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+    <?php
+        if($_SESSION['privileges']['admin'] || $_SESSION['privileges']['shipping']) {
+            echo "<a href = '../inventory_feature/index.php'> Update Inventory </a>";
+        }
+    ?>
+    </body>
+    </html>
+<?php
+
 }else{
     header('location: ../login');
 }
