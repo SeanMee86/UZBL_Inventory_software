3c42e11efb2ce6cfd9fc8c8e0c61a2e1a1caa3bb
SeanMee86 <seanmee8186@gmail.com>
2017-07-27 15:25:14 -0700

    sidebar updated, updated inventory updated"

--- components/header/header.php
+++ components/header/header.php
@@ -9,6 +9,9 @@
     <link rel="stylesheet" href="../components/styles/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
           integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.2/axios.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+    <script src="../inventory_feature/js/main.js"></script>
 </head>
 <body>
 <div class="header">
