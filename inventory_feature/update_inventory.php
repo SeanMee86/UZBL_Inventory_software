218814b2417b1b0bb6ff4531473dbdcac0bc126e
SeanMee86 <sean.mee86@yahoo.com>
2017-07-27 20:36:54 -0700

    fixed bug where quantity would still drop to 0 if amount greater than on hand was shipped

--- inventory_feature/update_inventory.php
+++ inventory_feature/update_inventory.php
@@ -19,32 +19,31 @@ $qty_on_hand = $select_qty_row['quantity'];
 
 //Query for updating the database inventory based on user info
 //$update_sql = "UPDATE `inventory` SET `quantity`=(SELECT `quantity` WHERE `upc`=$upc)-$qty WHERE `upc`=$upc";
-
-$update_sql = "UPDATE `inventory` SET `quantity`=$qty_on_hand-$qty WHERE `upc`=$upc";
+if($qty_on_hand > $qty) {
+    $update_sql = "UPDATE `inventory` SET `quantity`=$qty_on_hand-$qty WHERE `upc`=$upc";
 
 //Send query to the database
-$update_result = mysqli_query($conn, $update_sql);
+    $update_result = mysqli_query($conn, $update_sql);
 
 //Query for selecting updated inventory quantity
-$select_sql = "SELECT `quantity`,`name`,`device_model`,`color` FROM `inventory` WHERE `upc`=$upc";
+    $select_sql = "SELECT `quantity`,`name`,`device_model`,`color` FROM `inventory` WHERE `upc`=$upc";
 
 //Send query to the database
-$select_result = mysqli_query($conn, $select_sql);
+    $select_result = mysqli_query($conn, $select_sql);
 
 //If a result comes back add the data to the select data array and
-if($select_result) {
-    while ($select_row = mysqli_fetch_assoc($select_result)) {
-        $select_data[] = $select_row;
-    }
-    $item = $select_data[0]['name'];
-    $on_hand = $select_data[0]['quantity'];
-    $device = $select_data[0]['device_model'];
-    $color = $select_data[0]['color'];
-    if($qty > $on_hand){
-        echo 'not enough stock';
-    }else {
-        echo 'inventory successfully updated, new On Hand for the '. $item . ' for ' . $device .' ('.$color.')'.  ' = ' . $on_hand;
+    if ($select_result) {
+        while ($select_row = mysqli_fetch_assoc($select_result)) {
+            $select_data[] = $select_row;
+        }
+        $item = $select_data[0]['name'];
+        $on_hand = $select_data[0]['quantity'];
+        $device = $select_data[0]['device_model'];
+        $color = $select_data[0]['color'];
+        echo 'inventory successfully updated, new On Hand for the ' . $item . ' for ' . $device . ' (' . $color . ')' . ' = ' . $on_hand;
+    } else {
+        echo 'inventory update failed: ' . mysqli_error($conn);
     }
 }else{
-    echo 'inventory update failed: ' . mysqli_error($conn);
+    echo"Insufficient Inventory";
 }
