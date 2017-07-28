e72811af9510e259b2395751d12696f82beee791
SeanMee86 <seanmee8186@gmail.com>
2017-07-28 16:05:20 -0700

    working on search functionality for inventory

--- inventory_feature/index.php
+++ inventory_feature/index.php
@@ -2,33 +2,33 @@
 session_start();
 require"mysql_conf.php";
 if(isset($_SESSION['user_info'])){
-        include '../components/header/header.php';
-        include '../components/sidebar/sidebar.php'; ?>
-        <div class="row">
-            <input class="form-control col-4 inventory_search" type="text" placeholder="Enter Search Term">
-            <input class="btn btn-outline-success search_button" type="submit">
-        </div>
-        <br>
+    include '../components/header/header.php';
+    include '../components/sidebar/sidebar.php'; ?>
+    <form class="form-inline" action="inventory_search.php" method="post">
+        <input class="form-control col-6 inventory_search" type="text" name="search" placeholder="Enter Search Term">
+        <input class="btn btn-outline-success search_button" type="submit">
+    </form>
+    <br>
 
-        <?php
+    <?php
 
-        $sql = "SELECT `name`, `description`, `device_model`, `retail_price`, `quantity` FROM `inventory`";
+    $sql = "SELECT `name`, `description`, `device_model`, `retail_price`, `quantity` FROM `inventory`";
 
-        $result = mysqli_query($conn, $sql);
+    $result = mysqli_query($conn, $sql);
 
-        while($row = mysqli_fetch_assoc($result)){
-            $data[] = $row;
-        }
+    while($row = mysqli_fetch_assoc($result)){
+        $data[] = $row;
+    }
 
-        foreach($data as $key => $value){
-            $description = substr($value['description'], 0, 200);
-            echo '<div class="item_name">' . $value['name'] . ' for ' . $value['device_model'] . '</div>';
-            echo '<div class="item_description">' . $description . '...</div>';
-            echo '<div class="item_price">Price: ' . $value['retail_price'] . '</div>';
-            echo '<div class="item_quantity">Qty: ' . $value['quantity'] . '</div>';
-            echo '<br>';
-        }
-        include '../components/footer/footer.php';
+    foreach($data as $key => $value){
+        $description = substr($value['description'], 0, 200);
+        echo '<div class="item_name">' . $value['name'] . ' for ' . $value['device_model'] . '</div>';
+        echo '<div class="item_description">' . $description . '...</div>';
+        echo '<div class="item_price">Price: ' . $value['retail_price'] . '</div>';
+        echo '<div class="item_quantity">Qty: ' . $value['quantity'] . '</div>';
+        echo '<br>';
+    }
+    include '../components/footer/footer.php';
 }else{
     header('location: ../login/index.php');
 }
--- inventory_feature/inventory_search.php
+++ inventory_feature/inventory_search.php
@@ -1,36 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: seanm
- * Date: 7/28/2017
- * Time: 2:37 PM
- */
 session_start();
 require"mysql_conf.php";
 if(isset($_SESSION['user_info'])){
     include '../components/header/header.php';
     include '../components/sidebar/sidebar.php'; ?>
-    <div class="row">
-        <input class="form-control col-4 inventory_search" type="text" placeholder="Enter Search Term">
+    <form class="form-inline" action="inventory_search.php" method="post">
+        <input class="form-control col-6 inventory_search" type="text" name="search" placeholder="Enter Search Term">
         <input class="btn btn-outline-success search_button" type="submit">
-    </div>
+    </form>
     <br>
 
     <?php
 
-    $sql = "SELECT `name`, `description`, `device_model`, `retail_price`, `quantity` FROM `inventory` WHERE `tags`=";
+
+    $search_terms = explode(' ', $_POST["search"]);
+
+    $sql = "SELECT `name`, `description`, `device_model`, `retail_price`, `quantity` FROM `inventory` WHERE `tags` LIKE ";
+    $count = count($search_terms);
+    for($i = 0; $i<$count; $i++){
+        if($i === 0) {
+            $sql .= "'%" . $search_terms[$i] . "%'";
+        }else{
+            $sql .= " '%" . $search_terms[$i] . "%'";
+        }
+    }
 
     $result = mysqli_query($conn, $sql);
+    if($result) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $data[] = $row;
+        }
 
-    while($row = mysqli_fetch_assoc($result)){
-        $data[] = $row;
-    }
-    foreach($data as $key => $value){
-        echo '<div class="item_name">' . $value['name'] . ' for ' . $value['device_model'] . '</div>';
-        echo '<div class="item_description">' . $value['description'] . '</div>';
-        echo '<div class="item_price">Price: ' . $value['retail_price'] . '</div>';
-        echo '<div class="item_quantity">Qty: ' . $value['quantity'] . '</div>';
-        echo '<br>';
+        foreach ($data as $key => $value) {
+            $description = substr($value['description'], 0, 200);
+            echo '<div class="item_name">' . $value['name'] . ' for ' . $value['device_model'] . '</div>';
+            echo '<div class="item_description">' . $description . '...</div>';
+            echo '<div class="item_price">Price: ' . $value['retail_price'] . '</div>';
+            echo '<div class="item_quantity">Qty: ' . $value['quantity'] . '</div>';
+            echo '<br>';
+        }
     }
     include '../components/footer/footer.php';
 }else{
