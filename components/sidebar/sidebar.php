a6bf34011386c233098a727d49cf39d2ef9d08c1
SeanMee86 <seanmee8186@gmail.com>
2017-08-04 16:47:54 -0700

    cleaned up code added images and placeholders

--- components/sidebar/sidebar.php
+++ components/sidebar/sidebar.php
@@ -2,15 +2,20 @@
     <ul>
         <li class="sidebar_main"><a href="../dashboard">My Dashboard</a></li>
         <li class="sidebar_main" id="inventory_sidemenu"><a href="../inventory_main/index.php">Inventory</a>
-            <?php if(($_SERVER['PHP_SELF'] === "/bizurk_prototype/inventory_main/index.php" ||
-                $_SERVER['PHP_SELF'] === "/bizurk_prototype/ship_receive/index.php" ||
-                $_SERVER['PHP_SELF'] === "/bizurk_prototype/inventory_search/index.php") &&
-                ($_SESSION['privileges']['admin'] || $_SESSION['privileges']['shipping'])){;?>
-            <ul>
-                <li class="sidebar_sub">History</li>
-                <li class="sidebar_sub"><a href="../ship_receive/index.php">Updater</a></li>
-                <li class="sidebar_sub">New Product</li>
-            </ul>
+            <?php
+            $url = $_SERVER['PHP_SELF'];
+            $base_url = "/bizurk_prototype/";
+            $privileges = $_SESSION['privileges'];
+            if(($url === $base_url."inventory_main/index.php" ||
+                $url === $base_url."ship_receive/index.php" ||
+                $url === $base_url."inventory_search/index.php") &&
+                ($privileges['admin'] || $privileges['shipping'])){;
+            ?>
+                <ul>
+                    <li class="sidebar_sub">History</li>
+                    <li class="sidebar_sub"><a href="../ship_receive/index.php">Updater</a></li>
+                    <li class="sidebar_sub">New Product</li>
+                </ul>
             <?php }; ?>
         </li>
         <li class="sidebar_main">Messages</li>
